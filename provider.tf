29be593cf0cfed0af5a077f5430ad2465ee18113
Rajiv Gopal <84613366+rajivgopal0110@users.noreply.github.com>
2022-09-13 14:50:54 +0530

    Update and rename 00_provider_code.tf to provider.tf

--- provider.tf
+++ provider.tf
@@ -4,9 +4,3 @@ provider "vsphere" {
   vsphere_server       = var.vsphere_vcenter_server
   allow_unverified_ssl = true
 }
-
-provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_access_key
-  region     = var.aws_region
-}
